11b04f8fa0d8f132b7c6bde8568775cee84a945b
Berkin Berkcan Cirak <berkin@kuyabiye.com>
2016-10-04 21:35:15 +0300

    app structure

--- App.js
+++ App.js
@@ -5,7 +5,21 @@ class App extends React.Component {
         super();
     }
     render() {
-        return <div>xxx</div>;
+        return (
+            <header>
+              <div>Logo</div>
+              <ul>
+                <li><Link to={AboutUs} /></li>
+                <li><Link to={Categories} /></li>
+                <li><Link to={ContactUS} /></li>
+                <li><Link to={AboutUs} /></li>
+              </ul>
+            </header>
+            <section>this.props.children || <Home /></section>
+            <footer>
+
+            </footer>
+          );
     }
 }
 
--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {Router, Route, browserHistory} from 'react-router';
+import AboutUs from './components/AboutUs';
+import Categories from './components/Categories';
+import Product from './components/Product';
+import ContactUs from './components/ContactUs';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <Route path='/about-us' component={AboutUs} />
+
+      <Route path='/categories' component={Categories}>
+        <Route path='/:product-slug' component={Product} />
+      </Route>
+
+      <Route path='/contact-us' component={ContactUs} />
+    </Route>
+  </Router>
+), document.getElementById('app'));
