712a8a82b32c081e8fc1cb71f4b96953c220a870
Berkin <berkin@Berkins-MacBook-Pro.local>
2016-10-05 10:50:59 +0300

    structure

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router';
+import Home from './components/Home';
+import Login from './components/Login';
 
 class App extends React.Component {
     constructor() {
@@ -6,19 +9,24 @@ class App extends React.Component {
     }
     render() {
         return (
+          <div>
             <header>
               <div>Logo</div>
               <ul>
-                <li><Link to={AboutUs} /></li>
-                <li><Link to={Categories} /></li>
-                <li><Link to={ContactUS} /></li>
-                <li><Link to={AboutUs} /></li>
+                <li><Link to='/about-us'>About Us</Link></li>
+                <li><Link to='/categories'>Categories</Link></li>
+                <li><Link to='/contact-us'>Contact Us</Link></li>
+              </ul>
+              
+              <ul> 
+                <li><Link to='/login'>Login</Link></li>
               </ul>
             </header>
-            <section>this.props.children || <Home /></section>
+            <section>{this.props.children || <Home />}</section>
             <footer>
-
+              Foot note
             </footer>
+          </div>
           );
     }
 }
