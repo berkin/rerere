8f70dadefbc383db4c7facf8331d6d1ebb327bd1
berkin <berkin@kuyabiye.com>
2016-10-05 17:20:20 +0300

    home

---
+++ components/FeaturedProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class FeaturedProduct extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <div>FeaturedProduct</div>;
+    }
+}
+
+export default FeaturedProduct;
---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <div>Sidebar</div>;
+    }
+}
+
+export default Sidebar;
