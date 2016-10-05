43ecfc94284e4ffc6fe416d02240fb073d2aba8e
Berkin <berkin@Berkins-MacBook-Pro.local>
2016-10-05 17:39:17 +0300

    featured product

--- components/FeaturedProduct.js
+++ components/FeaturedProduct.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class FeaturedProduct extends React.Component {
+
     constructor() {
         super();
+
+        this.state = {
+			img: {
+				src: 'featured.jpg',
+				alt: 'featured'
+			},
+			title: 'Featured Product',
+			excerpt: 'Featured product excerpt'
+		};
     }
+
     render() {
-        return <div>FeaturedProduct</div>;
+        return (
+        	<div>
+        		<img src={this.state.img.src} alt={this.state.img.alt} />
+        		<h1>{this.state.title}</h1>
+        		<p>{this.state.excerpt}</p>
+        	</div>
+        );
     }
 }
 
