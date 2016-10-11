4727c260e7be7b158c330f5e3a99177535353f82
berkin <berkin.cirak@comeon.com>
2016-10-11 17:53:15 +0300

    category

---
+++ components/Category.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Category extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <div>Category</div>;
+    }
+}
+
+export default Category;
